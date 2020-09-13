b9f148809abb0232d3a6efaead0e66c55d728663
Maurício Neto <malbuquerqueneto@gmail.com>
2020-09-13 03:01:37 -0300

    fix: removing unused imports

--- src/main/java/com/safra/safrat6/controller/AccountController.java
+++ src/main/java/com/safra/safrat6/controller/AccountController.java
@@ -1,6 +1,5 @@
 package com.safra.safrat6.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
